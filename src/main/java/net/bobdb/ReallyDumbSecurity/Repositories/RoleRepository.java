33bad338e932deeba86d19eddebd842786ed7aa7
bobdb <bobdb40@gmail.com>
2021-06-08 23:29:31 -0400

    added Entities and Respositories

---
+++ src/main/java/net/bobdb/ReallyDumbSecurity/Repositories/RoleRepository.java
@@ -0,0 +1,14 @@
+package net.bobdb.ReallyDumbSecurity.Repositories;
+
+import java.util.Optional;
+
+import net.bobdb.ReallyDumbSecurity.entities.ERole;
+import net.bobdb.ReallyDumbSecurity.entities.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Optional<Role> findByName(ERole name);
+}
